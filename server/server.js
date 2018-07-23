8e897e528589dc60179cb2414585de0dd5ae915f
aespinet <aelluvetaa@gmail.com>
2018-07-23 12:09:07 -0400

    Restaurant Data Deploy 04

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 
 app.use( bodyParser.json() );
 app.use( cors({
-  origin: 'https://reactiverestaurant.herokuapp.com/'
+  origin: 'https://reactiverestaurant.herokuapp.com'
 }) );
 
 // requests
